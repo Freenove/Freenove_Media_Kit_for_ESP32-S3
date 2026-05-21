1e9b51d32e1e88971a46b3a299bccafb4c17807b
Zhentao-Lin <173039613@qq.com>
2026-05-21 17:17:12 +0800

    Update Sketch_07_Video_Web_Server.ino

--- Sketches/Sketch_07_Video_Web_Server/Sketch_07_Video_Web_Server.ino
+++ Sketches/Sketch_07_Video_Web_Server/Sketch_07_Video_Web_Server.ino
@@ -66,75 +66,70 @@ void loop() {
 }
 
 void camera_init(void) {
- camera_config_t config;
- config.ledc_channel = LEDC_CHANNEL_0;
- config.ledc_timer = LEDC_TIMER_0;
- config.pin_d0 = Y2_GPIO_NUM;
- config.pin_d1 = Y3_GPIO_NUM;
- config.pin_d2 = Y4_GPIO_NUM;
- config.pin_d3 = Y5_GPIO_NUM;
- config.pin_d4 = Y6_GPIO_NUM;
- config.pin_d5 = Y7_GPIO_NUM;
- config.pin_d6 = Y8_GPIO_NUM;
- config.pin_d7 = Y9_GPIO_NUM;
- config.pin_xclk = XCLK_GPIO_NUM;
- config.pin_pclk = PCLK_GPIO_NUM;
- config.pin_vsync = VSYNC_GPIO_NUM;
- config.pin_href = HREF_GPIO_NUM;
- config.pin_sccb_sda = SIOD_GPIO_NUM;
- config.pin_sccb_scl = SIOC_GPIO_NUM;
- config.pin_pwdn = PWDN_GPIO_NUM;
- config.pin_reset = RESET_GPIO_NUM;
- config.xclk_freq_hz = 10000000;
- config.frame_size = FRAMESIZE_VGA;
- config.pixel_format = PIXFORMAT_RGB565;  // for streaming
- config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
- config.fb_location = CAMERA_FB_IN_PSRAM;
- config.jpeg_quality = 12;
- config.fb_count = 1;
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000;
+  config.frame_size = FRAMESIZE_QVGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
 
- // If PSRAM IC is present, use higher resolution and quality
- if (psramFound()) {
-   config.jpeg_quality = 10;
-   config.fb_count = 2;
-   config.grab_mode = CAMERA_GRAB_LATEST;
- } else {
-   // Limit the frame size when PSRAM is not available
-   config.frame_size = FRAMESIZE_VGA;
-   config.fb_location = CAMERA_FB_IN_DRAM;
- }
-
- // Initialize the camera with the specified configuration
- esp_err_t err = esp_camera_init(&config);
- if (err != ESP_OK) {
-   Serial.printf("Camera init failed with error 0x%x", err);
-   return;
- }
-
- // Get the camera sensor and adjust settings
- sensor_t* s = esp_camera_sensor_get();
- uint8_t pid = s->id.PID;
-
- if(pid == 0x45)
- {
-   s->set_hmirror(s, 0);
-   vTaskDelay(500);
-   s->set_vflip(s, 0);       // Flip the image vertically
- }else if(pid == 0x26)
- {
-   s->set_hmirror(s, 1);
-   s->set_vflip(s, 1);       // Flip the image vertically
- }else if(pid == 0x9B)
- {
-   s->set_hmirror(s, 1);
-   vTaskDelay(500);
-   s->set_vflip(s, 1);       // Flip the image vertically
- }
- else{
-   s->set_hmirror(s, 1);
-   s->set_vflip(s, 0);       // Flip the image vertically
- }
- s->set_brightness(s, 1);  // Slightly increase brightness
- s->set_saturation(s, 0);  // Reduce saturation
- s->set_ae_level(s, -3);   // Set exposure compensation level
+  // Initialize the camera with the specified configuration
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    if(err==ESP_ERR_NOT_SUPPORTED){
+      config.pixel_format = PIXFORMAT_RGB565;
+      esp_err_t err = esp_camera_init(&config);
+      if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+      }
+    }
+  }
+  sensor_t* s = esp_camera_sensor_get();
+  uint16_t pid = s->id.PID;
+  if(pid == OV2640_PID){
+    s->set_hmirror(s, 0);
+    s->set_vflip(s, 0);     
+  }
+  else if(pid == OV3660_PID){
+    s->set_hmirror(s, 0);
+    s->set_vflip(s, 1);     
+  }
+  else if(pid == GC2145_PID){
+    s->set_hmirror(s, 1);
+    delay(500);
+    s->set_vflip(s, 1);      
+  }
+  else if(pid == GC0308_PID){
+    s->set_hmirror(s, 0);
+    delay(500);
+    s->set_vflip(s, 0);     
+  }
+  else{
+    s->set_hmirror(s, 0);
+    s->set_vflip(s, 1);       
+  }
+  s->set_brightness(s, 1);  // Slightly increase brightness
+  s->set_saturation(s, 0);  // Reduce saturation
+  s->set_ae_level(s, -3);   // Set exposure compensation level
 }
